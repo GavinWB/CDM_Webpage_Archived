97d7d53e18c70b6000fcac4c1d9e656ac7e9c7d4
hans-ict <tuannguyendinh224@gmail.com>
2019-07-16 11:56:13 +0900

    + Create exam component

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { HomeComponent } from './components/home/home.component';
+import { ExamComponent } from './components/exam/exam.component';
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
   {path: 'home', component: HomeComponent},
+  {path: 'exam', component: ExamComponent},
   {path: "**", redirectTo: "login"}
 ];
 
--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
+import { UserService } from './services/user.service';
+import { QuestionService } from './services/question.service';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
@@ -10,9 +13,7 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { ModalComponent } from './components/modal/modal.component';
 import { RegisterComponent } from './components/register/register.component';
 import { HomeComponent } from './components/home/home.component';
-
-import { UserService } from './services/user.service';
-import { QuestionService } from './services/question.service';
+import { ExamComponent } from './components/exam/exam.component';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import { QuestionService } from './services/question.service';
     NavbarComponent,
     ModalComponent,
     RegisterComponent,
-    HomeComponent
+    HomeComponent,
+    ExamComponent
   ],
   imports: [
     BrowserModule,
