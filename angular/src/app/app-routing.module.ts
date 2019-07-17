bfbb10646220d13b3210e4abbe4fe73e3eaf0c81
hans-ict <tuannguyendinh224@gmail.com>
2019-07-17 14:40:40 +0900

    + Add guards to prevent incorrect routings

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AuthGuard } from './guards/auth.guard';
+import { NotauthGuard } from './guards/notauth.guard';
+
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { HomeComponent } from './components/home/home.component';
 import { ExamComponent } from './components/exam/exam.component';
 
 const routes: Routes = [
-  {path: 'login', component: LoginComponent},
-  {path: 'register', component: RegisterComponent},
-  {path: 'home', component: HomeComponent},
-  {path: 'exam', component: ExamComponent},
+  {path: 'login', component: LoginComponent, canActivate: [NotauthGuard]},
+  {path: 'register', component: RegisterComponent, canActivate: [NotauthGuard]},
+  {path: 'home', component: HomeComponent, canActivate: [AuthGuard]},
+  {path: 'exam', component: ExamComponent, canActivate: [AuthGuard]},
   {path: "**", redirectTo: "login"}
 ];
 
